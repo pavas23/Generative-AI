b2bc000993e763e93b01619c067afd38e90efe02
Pavas Garg <pavasgarg@Pavass-MacBook-Pro.local>
2024-04-25 18:48:22 +0530

    added dcgan model

---
+++ PART_B/DeepConvGAN/model.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    # discriminator takes a 64 * 64 image and downsamples the dimensions while increasing the number of channels to capture higher level features
+    def __init__(self, channels_img, features_d):
+        # as this class is inheriting from nn.Module class
+        super(Discriminator, self).__init__()
+
+        self.disc = nn.Sequential(
+            # input: N x channels_img x 64 x 64
+            nn.Conv2d(channels_img, features_d, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            # _block(in_channels, out_channels, kernel_size, stride, padding)
+            # in_channels refers to number of channels in input feature map
+            # out_channles refers to number of filters or feature maps produced after the conv operation
+            self._block(features_d, features_d * 2, 4, 2, 1),
+            self._block(features_d * 2, features_d * 4, 4, 2, 1),
+            self._block(features_d * 4, features_d * 8, 4, 2, 1),
+            # After all _block img output is 4x4 (Conv2d below makes into 1x1)
+            nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=2, padding=0),
+            nn.Sigmoid(),
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2),
+        )
+
+    def forward(self, x):
+        return self.disc(x)
+
+
+class Generator(nn.Module):
+    # generator takes a noise vector as input and transforms it into a fake img with desired number of channels and dimensions
+    def __init__(self, channels_noise, channels_img, features_g):
+        super(Generator, self).__init__()
+
+        # channels_noise is the number of channels in the input noise vector
+        # channels_img is the number of channels in the output generated image
+        self.net = nn.Sequential(
+            # Input: N x channels_noise x 1 x 1
+            self._block(channels_noise, features_g * 16, 4, 1, 0),  # img: 4x4
+            self._block(features_g * 16, features_g * 8, 4, 2, 1),  # img: 8x8
+            self._block(features_g * 8, features_g * 4, 4, 2, 1),  # img: 16x16
+            self._block(features_g * 4, features_g * 2, 4, 2, 1),  # img: 32x32
+            nn.ConvTranspose2d(
+                features_g * 2, channels_img, kernel_size=4, stride=2, padding=1
+            ),
+            # Output: N x channels_img x 64 x 64
+            nn.Tanh(),
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+def initialize_weights(model):
+    # Initializes weights according to the DCGAN paper
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            # weights are initialized from a zero-centered normal with standard deviation of 0.03
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+
---
+++ PART_B/DeepConvGAN/split_dataset.py
@@ -0,0 +1,56 @@
+import os
+import random
+from torchvision.datasets import ImageFolder
+from torch.utils.data import Subset
+import shutil
+
+# make hash map with image name and label (for train, validation and split set)
+def read_split_annotations(file_path):
+    split_annotations = {}
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            parts = line.strip().split()
+            image_name = parts[0]
+            split_label = int(parts[1])
+            split_annotations[image_name] = split_label
+    return split_annotations
+
+# split dataset according to labels
+def split_dataset(root_dir, partition_file):
+    split_annotations = read_split_annotations(os.path.join(root_dir, partition_file))
+
+    # splitting images into 3 separate sets
+    train_img_names = [image_name for image_name, split_label in split_annotations.items() if split_label == 0]
+    val_img_names = [image_name for image_name, split_label in split_annotations.items() if split_label == 1]
+    test_img_names = [image_name for image_name, split_label in split_annotations.items() if split_label == 2]
+
+    # shuffling indices
+    random.shuffle(train_img_names)
+    random.shuffle(val_img_names)
+    random.shuffle(test_img_names)
+
+    return train_img_names, val_img_names, test_img_names
+
+def copy_images(source_dir, image_names, destination_dir):
+    for image_name in image_names:
+        print(image_name)
+        source_path = os.path.join(source_dir, image_name)
+        print(source_path)
+        destination_path = os.path.join(destination_dir,image_name)
+        print(destination_path)
+        shutil.copyfile(source_path, destination_path)
+
+root_dir = "dataset"
+partition_file = "list_eval_partition.txt"
+train_img_names, val_img_names, test_img_names = split_dataset(root_dir,partition_file)
+
+source_dir = "./dataset/celeba/img_align_celeba"
+destination_dir_train = "./dataset/celeba_train/img_align_celeba"
+destination_dir_val = "./dataset/celeba_val/img_align_celeba"
+destination_dir_test = "./dataset/celeba_test/img_align_celeba"
+
+# this will copy images to respective directories
+copy_images(source_dir,train_img_names,destination_dir_train)
+copy_images(source_dir,val_img_names,destination_dir_val)
+copy_images(source_dir,test_img_names,destination_dir_test)
