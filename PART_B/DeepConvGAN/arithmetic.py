832e9537ec4fa9991399723cbd9e5d54ec85dbd7
avadoe <aashuaripirala@gmail.com>
2024-04-30 23:13:23 +0530

    arithmetic

--- PART_B/DeepConvGAN/arithmetic.py
+++ PART_B/DeepConvGAN/arithmetic.py
@@ -1,11 +1,7 @@
 import random
 import torch
-import torch.nn as nn
 import torch.nn.parallel
-import torch.optim as optim
 import torch.utils.data
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 from model import Encoder, Generator
 from split_dataset import (
@@ -21,139 +17,155 @@ from split_dataset import (
     people_no_mus,
 )
 
-
-def main():
-    manualSeed = 69
-    random.seed(manualSeed)
-    torch.manual_seed(manualSeed)
+def initialize_seed(seed):
+    random.seed(seed)
+    torch.manual_seed(seed)
     torch.use_deterministic_algorithms(True)
 
-    device = torch.device("cuda:1")
+
+def initialize_models(device):
     netG = Generator().to(device)
     netG.load_state_dict(torch.load("./checkpoints/generator.pth"))
     netG.eval()
+
     encoder = Encoder().to(device)
     encoder.load_state_dict(torch.load("./checkpoints/encoder.pth"))
     encoder.eval()
 
+    return netG, encoder
+
+def plot_images(input_sets, file_name, subplot_shape, figsize=(9, 15)):
+    fig, axs = plt.subplots(subplot_shape[0], subplot_shape[1], figsize=figsize)
+    for i, input_set in enumerate(input_sets):
+        for j, image_tensor in enumerate(input_set):
+            image = image_tensor.detach().cpu().numpy().transpose(1, 2, 0)
+            axs[i, j].imshow(image)
+            axs[i, j].axis("off")
+    plt.tight_layout()
+    plt.savefig(f"results_fin/{file_name}.png")
+    plt.close()
+
+
+def generate_and_plot_images(
+    device, netG, encoder, input_data, operations, file_name, subplot_shape
+):
     insides = []
     outsides = []
+    input_sets = []
+
     for i in range(5):
-        print(men_no_glasses[i].shape)
-        print(encoder(men_no_glasses[i]).shape)
-        inside = (
+        inside_code = operations["inside"](i)
+        outside_code = operations["outside"](i)
+
+        inside_image = netG(inside_code)
+        outside_image = netG(outside_code)
+
+        insides.append(inside_image)
+        outsides.append(outside_image)
+
+        input_sets.append(input_data["inputs"][i] + [insides[i], outsides[i]])
+
+    plot_images(input_sets, file_name, subplot_shape)
+
+
+def main():
+    initialize_seed(69)
+
+    device = torch.device("cuda:1")
+    netG, encoder = initialize_models(device)
+
+    operations_men_with_glasses = {
+        "inside": lambda i: (
             encoder(men_no_glasses[i])
             + encoder(people_with_glasses[i])
             - encoder(people_no_glasses[i])
-        )
-        outside = encoder(
+        ),
+        "outside": lambda i: (
             men_no_glasses[i] + people_with_glasses[i] - people_no_glasses[i]
-        )
-        inside_image = netG(inside)
-        outside_image = netG(outside)
-        print(inside_image.shape)
-        print(outside_image.shape)
-        insides.append(inside_image)
-        outsides.append(outside_image)
+        ),
+    }
 
-    fig, axs = plt.subplots(5, 5, figsize=(9, 15))
-    for i in range(5):
-        axs[i, 0].imshow(men_no_glasses[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 0].axis("off")
-        axs[i, 1].imshow(
-            people_with_glasses[i].detach().cpu().numpy().transpose(1, 2, 0)
-        )
-        axs[i, 1].axis("off")
-        axs[i, 2].imshow(people_no_glasses[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 2].axis("off")
-        axs[i, 3].imshow(insides[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 3].axis("off")
-        axs[i, 4].imshow(outsides[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 4].axis("off")
-    plt.tight_layout()
-    plt.savefig(f"results_fin/men_with_glass.png")
-    plt.close()
+    input_data_men_with_glasses = {
+        "inputs": [
+            [men_no_glasses[i], people_with_glasses[i], people_no_glasses[i]]
+            for i in range(5)
+        ]
+    }
+    generate_and_plot_images(
+        device,
+        netG,
+        encoder,
+        input_data_men_with_glasses,
+        operations_men_with_glasses,
+        "men_with_glass",
+        (5, 5),
+    )
 
-    insides = []
-    outsides = []
-    for i in range(5):
-        print(men_no_glasses[i].shape)
-        print(encoder(men_no_glasses[i]).shape)
-        inside = (
+    operations_women_with_glasses = {
+        "inside": lambda i: (
             encoder(men_with_glasses[i])
             - encoder(men_no_glasses[i])
             + encoder(women_no_glasses[i])
-        )
-        outside = encoder(men_with_glasses[i] - men_no_glasses[i] + women_no_glasses[i])
-        inside_image = netG(inside)
-        outside_image = netG(outside)
-        print(inside_image.shape)
-        print(outside_image.shape)
-        insides.append(inside_image)
-        outsides.append(outside_image)
+        ),
+        "outside": lambda i: (
+            men_with_glasses[i] - men_no_glasses[i] + women_no_glasses[i]
+        ),
+    }
 
-    fig, axs = plt.subplots(5, 5, figsize=(9, 15))
-    for i in range(5):
-        axs[i, 0].imshow(men_with_glasses[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 0].axis("off")
-        axs[i, 1].imshow(men_no_glasses[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 1].axis("off")
-        axs[i, 2].imshow(women_no_glasses[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 2].axis("off")
-        axs[i, 3].imshow(insides[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 3].axis("off")
-        axs[i, 4].imshow(outsides[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 4].axis("off")
-    plt.tight_layout()
-    plt.savefig(f"results_fin/women_with_glass.png")
-    plt.close()
-
-    insides = []
-    outsides = []
-    for i in range(5):
-        print(men_with_smile[i].shape)
-        print(encoder(men_with_smile[i]).shape)
-        inside = (
+    input_data_women_with_glasses = {
+        "inputs": [
+            [men_with_glasses[i], men_no_glasses[i], women_no_glasses[i]]
+            for i in range(5)
+        ]
+    }
+    generate_and_plot_images(
+        device,
+        netG,
+        encoder,
+        input_data_women_with_glasses,
+        operations_women_with_glasses,
+        "women_with_glass",
+        (5, 5),
+    )
+    
+    operations_men_with_hat_smile_mustache = {
+        "inside": lambda i: (
             encoder(men_with_smile[i])
             + encoder(people_with_hat[i])
             - encoder(people_no_hat[i])
             + encoder(people_with_mus[i])
             - encoder(people_no_mus[i])
-        )
-        outside = encoder(
+        ),
+        "outside": lambda i: (
             men_with_smile[i]
             + people_with_hat[i]
             - people_no_hat[i]
             + people_with_mus[i]
             - people_no_mus[i]
-        )
-        inside_image = netG(inside)
-        outside_image = netG(outside)
-        print(inside_image.shape)
-        print(outside_image.shape)
-        insides.append(inside_image)
-        outsides.append(outside_image)
+        ),
+    }
 
-    fig, axs = plt.subplots(5, 7, figsize=(9, 21))
-    for i in range(5):
-        axs[i, 0].imshow(men_with_smile[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 0].axis("off")
-        axs[i, 1].imshow(people_with_hat[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 1].axis("off")
-        axs[i, 2].imshow(people_no_hat[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 2].axis("off")
-        axs[i, 3].imshow(people_with_mus[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 3].axis("off")
-        axs[i, 4].imshow(people_no_mus[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 4].axis("off")
-        axs[i, 5].imshow(insides[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 5].axis("off")
-        axs[i, 6].imshow(outsides[i].detach().cpu().numpy().transpose(1, 2, 0))
-        axs[i, 6].axis("off")
-    plt.tight_layout()
-    plt.savefig(f"results_fin/men_with_hat_smile_mustache.png")
-    plt.close()
-
+    input_data_men_with_hat_smile_mustache = {
+        "inputs": [
+            [
+                men_with_smile[i],
+                people_with_hat[i],
+                people_no_hat[i],
+                people_with_mus[i],
+                people_no_mus[i],
+            ]
+            for i in range(5)
+        ]
+    }
+    generate_and_plot_images(
+        device,
+        netG,
+        encoder,
+        input_data_men_with_hat_smile_mustache,
+        operations_men_with_hat_smile_mustache,
+        "men_with_hat_smile_mustache",
+        (5, 7),
+    )
 
 if __name__ == "__main__":
     main()
