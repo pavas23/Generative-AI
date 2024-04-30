569dc62ea56ca127320138669b2ab1ff3111209f
avadoe <aashuaripirala@gmail.com>
2024-04-30 22:02:07 +0530

    train change

--- PART_B/DeepConvGAN/model.py
+++ PART_B/DeepConvGAN/model.py
@@ -1,143 +1,68 @@
 import torch.nn as nn
 
-# GAN Discriminator
 class Discriminator(nn.Module):
-    def __init__(self,ngpu,FEATURES_DISC,CHANNELS_IMG):
+    def __init__(self):
         super(Discriminator, self).__init__()
-        self.ngpu = ngpu
         self.main = nn.Sequential(
-            # input is ``(CHANNELS_IMG) x 64 x 64``
-            nn.Conv2d(CHANNELS_IMG, FEATURES_DISC, 4, 2, 1, bias=False),
+            nn.Conv2d(3, 64, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(FEATURES_DISC) x 32 x 32``
-            nn.Conv2d(FEATURES_DISC, FEATURES_DISC * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(FEATURES_DISC * 2),
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(FEATURES_DISC*2) x 16 x 16``
-            nn.Conv2d(FEATURES_DISC * 2, FEATURES_DISC * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(FEATURES_DISC * 4),
+            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(FEATURES_DISC*4) x 8 x 8``
-            nn.Conv2d(FEATURES_DISC * 4, FEATURES_DISC * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(FEATURES_DISC * 8),
+            nn.Conv2d(256, 512, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(FEATURES_DISC*8) x 4 x 4``
-            nn.Conv2d(FEATURES_DISC * 8, 1, 4, 1, 0, bias=False),
-            nn.Sigmoid()
+            nn.Conv2d(512, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid(),
         )
 
     def forward(self, input):
         return self.main(input)
 
-
-# GAN Generator
 class Generator(nn.Module):
-    def __init__(self, ngpu, NOISE_DIM,FEATURES_GEN,CHANNELS_IMG):
+    def __init__(self):
         super(Generator, self).__init__()
-        self.ngpu = ngpu
         self.main = nn.Sequential(
-            # input is Z, going into a convolution
-            nn.ConvTranspose2d( NOISE_DIM, FEATURES_GEN * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(FEATURES_GEN * 8),
+            nn.ConvTranspose2d(100, 512, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(512),
             nn.ReLU(True),
-            # state size. ``(FEATURES_GEN*8) x 4 x 4``
-            nn.ConvTranspose2d(FEATURES_GEN * 8, FEATURES_GEN * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(FEATURES_GEN * 4),
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
             nn.ReLU(True),
-            # state size. ``(FEATURES_GEN*4) x 8 x 8``
-            nn.ConvTranspose2d( FEATURES_GEN * 4, FEATURES_GEN * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(FEATURES_GEN * 2),
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
             nn.ReLU(True),
-            # state size. ``(FEATURES_GEN*2) x 16 x 16``
-            nn.ConvTranspose2d( FEATURES_GEN * 2, FEATURES_GEN, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(FEATURES_GEN),
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
             nn.ReLU(True),
-            # state size. ``(FEATURES_GEN) x 32 x 32``
-            nn.ConvTranspose2d( FEATURES_GEN, CHANNELS_IMG, 4, 2, 1, bias=False),
-            nn.Tanh()
-            # state size. ``(CHANNELS_IMG) x 64 x 64``
+            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
+            nn.Tanh(),
         )
 
     def forward(self, input):
-        return self.main(input)
-
-# custom weights initialization called for generator and discriminator
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
-
-# CNN Encoder model
-# class Encoder(nn.Module):
-#     def __init__(self, channels_img, features_gen, noise_dim):
-#         super(Encoder, self).__init__()
-
-#         self.encoder = nn.Sequential(
-#             # layer 1, input img: 64 * 64
-#             nn.Conv2d(channels_img, features_gen, kernel_size=4, stride=2, padding=1),
-#             nn.LeakyReLU(0.2),
-#             nn.BatchNorm2d(features_gen),
-#             # layer 2, input img: 32 * 32
-#             nn.Conv2d(features_gen, features_gen*2, kernel_size=4, stride=2, padding=1),
-#             nn.LeakyReLU(0.2),
-#             nn.BatchNorm2d(features_gen*2),
-#             # layer 3, input img: 16 * 16
-#             nn.Conv2d(features_gen*2, features_gen*4, kernel_size=4, stride=2, padding=1),
-#             nn.LeakyReLU(0.2),
-#             nn.BatchNorm2d(features_gen*4),
-#             # layer 4, input img: 8 * 8
-#             nn.Conv2d(features_gen*4, features_gen*8, kernel_size=4, stride=2, padding=1),
-#             nn.LeakyReLU(0.2),
-#             nn.BatchNorm2d(features_gen*8),
-#             # layer 5, input img: 4 * 4
-#             nn.Conv2d(features_gen*8, features_gen*16, kernel_size=4, stride=2, padding=1),
-#             nn.LeakyReLU(0.2),
-#             nn.BatchNorm2d(features_gen*16),
-#             # layer 6, input img: 2 * 2
-#             nn.Conv2d(features_gen*16, features_gen*32, kernel_size=4, stride=2, padding=1),
-#             nn.LeakyReLU(0.2),
-#             nn.BatchNorm2d(features_gen*32),
-#             # layer 7, noise of dim 1000
-#             nn.Conv2d(features_gen*32, noise_dim, kernel_size=2, stride=1, padding=0),  # Adjusted kernel size
-#             nn.Flatten()
-#         )
-
-#     def forward(self, x):
-#         return self.encoder(x)
+        return self.main(input) / 2 + 0.5
 
 class Encoder(nn.Module):
-    def __init__(self, channels, features_gen, noise_dim):
+    def __init__(self):
         super(Encoder, self).__init__()
-        
-        # write the encoder nn.sequential with 6 conv layers, leaky relu and batch norm 
         self.encoder = nn.Sequential(
-            # layer 1, input img: 64 * 64
-            nn.Conv2d(channels, features_gen, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.BatchNorm2d(features_gen),
-            # layer 2, input img: 32 * 32
-            nn.Conv2d(features_gen, features_gen*2, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.BatchNorm2d(features_gen*2),
-            # layer 3, input img: 16 * 16
-            nn.Conv2d(features_gen*2, features_gen*4, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.BatchNorm2d(features_gen*4),
-            # layer 4, input img: 8 * 8
-            nn.Conv2d(features_gen*4, features_gen*8, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.BatchNorm2d(features_gen*8),
-            # layer 5, input img: 4 * 4
-            nn.Conv2d(features_gen*8, features_gen*16, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.BatchNorm2d(features_gen*16),
-            # layer 6, input img: 2 * 2
-            nn.Conv2d(features_gen*16, noise_dim, kernel_size=2, stride=1, padding=0),  # Adjusted kernel size
-            nn.Flatten()
+            nn.Conv2d(3, 64, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(256, 512, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(512, 100, 4, 1, 0, bias=False),
+            nn.Flatten(),
         )
-        
+
     def forward(self, x):
         return self.encoder(x)
--- PART_B/DeepConvGAN/train_encoder.py
+++ PART_B/DeepConvGAN/train_encoder.py
@@ -6,153 +6,94 @@ import torch.optim as optim
 import torch.utils.data
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
-import torchvision.utils as vutils
-import numpy as np
 import matplotlib.pyplot as plt
-import multiprocessing
-from model import Encoder,Generator
+from model import Encoder, Generator
+
 
 def main():
-    # set random seed for reproducibility, so that when everytime code is run, same results are produced
-    manualSeed = 999
+    manualSeed = 69
     random.seed(manualSeed)
     torch.manual_seed(manualSeed)
     torch.use_deterministic_algorithms(True)
 
-    # defining hyperparameters (as given in DCGAN paper)
-    LEARNING_RATE = 0.0002 
-    BATCH_SIZE = 128 # mini-batch SGD
-    IMAGE_SIZE = 64
-    CHANNELS_IMG = 3 # for RGB
-    NOISE_DIM = 100
-    NUM_EPOCHS = 20
-    FEATURES_DISC = 64
-    FEATURES_GEN = 64
+    dataset_train = dset.ImageFolder(
+        root="../T/dataset",
+        transform=transforms.Compose(
+            [
+                transforms.Resize(64),
+                transforms.CenterCrop(64),
+                transforms.ToTensor(),
+            ]
+        ),
+    )
 
-    # number of workers for dataloader
-    workers = 2
-    # Beta1 hyperparameter for Adam optimizers
-    beta1 = 0.5
-    # Number of GPUs available. Use 0 for CPU mode.
-    ngpu = 1
+    dataset_val = dset.ImageFolder(
+        root="../T/dataset",
+        transform=transforms.Compose(
+            [
+                transforms.Resize(64),
+                transforms.CenterCrop(64),
+                transforms.ToTensor(),
+            ]
+        ),
+    )
 
-    # Initialize the loss (binary cross entropy) function
-    criterion = nn.BCELoss()
+    dataloader = torch.utils.data.DataLoader(dataset_train, batch_size=64, shuffle=True)
+    dataloader_val = torch.utils.data.DataLoader(
+        dataset_val, batch_size=64, shuffle=True
+    )
 
-    # selecting the device
-    device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
-
-    # pre-processing images, creates a pipeline for transforming images using dataloading step
-    # taking dataset from local directory
-    dataset_train = dset.ImageFolder(root="./dataset",transform=transforms.Compose([
-                                transforms.Resize(IMAGE_SIZE),
-                                transforms.CenterCrop(IMAGE_SIZE),
-                                transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                            ]))
-    
-    dataset_val = dset.ImageFolder(root="./dataset",transform=transforms.Compose([
-                                transforms.Resize(IMAGE_SIZE),
-                                transforms.CenterCrop(IMAGE_SIZE),
-                                transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                            ]))
-    dataset_test = dset.ImageFolder(root="./dataset",transform=transforms.Compose([
-                                transforms.Resize(IMAGE_SIZE),
-                                transforms.CenterCrop(IMAGE_SIZE),
-                                transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                            ]))
-    
-    # create the dataloader
-    dataloader = torch.utils.data.DataLoader(dataset_train, batch_size=BATCH_SIZE,shuffle=True, num_workers=workers)
-    dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=BATCH_SIZE,shuffle=True, num_workers=workers)
-    dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=BATCH_SIZE,shuffle=True, num_workers=workers)
-
-    # training the encoder for the given generator, for generating z vectors
-    encoder = Encoder(CHANNELS_IMG, FEATURES_GEN, NOISE_DIM).to(device)
-
-    # defining optimizer for encoder cnn
-    opt_enc = optim.Adam(encoder.parameters())
-    criterion_enc = nn.MSELoss()
-
-    # defining best parameters for encoder
-    best_loss = float('inf')
-    best_lr = None
-    best_beta1 = None
-
-    # trying on different learning rates and beta1_values
-    LEARNING_RATES = [0.0001, 0.0002, 0.0005]
-    BETA1_VALUES = [0.5, 0.9]
-
-    # loading trained generator model
-    netG = Generator(ngpu,NOISE_DIM,FEATURES_GEN,CHANNELS_IMG,).to(device)
-    netG.load_state_dict(torch.load("./trained_models/generator.pth", map_location=device))
+    device = torch.device("cuda:1")
+    netG = Generator().to(device)
+    netG.load_state_dict(torch.load("./checkpoints/generator.pth"))
     netG.eval()
 
-    for lr in LEARNING_RATES:
-        for beta1 in BETA1_VALUES:
-            optimizer = optim.Adam(encoder.parameters(),lr=lr,betas=(beta1,0.999))
+    criterion = nn.MSELoss()
+    encoder = Encoder().to(device)
+    optimizer = optim.Adam(encoder.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    print("Starting Training Loop...")
+    for epoch in range(10):
+        # encoder.train()
+        # total_loss = 0
 
-            for epoch in range(NUM_EPOCHS):
-                encoder.train()
-                total_loss = 0
+        # for batch_idx, (real, _) in enumerate(dataloader):
+        #     real = real.to(device)
+        #     noise = encoder(real).view(-1, 100, 1, 1)
+        #     fake = netG(noise)
+        #     loss = criterion(real, fake)
+        #     total_loss += loss.item()
 
-                for batch_idx, (real, _) in enumerate(dataloader):
-                    real = real.to(device)
-                    optimizer.zero_grad()
+        #     optimizer.zero_grad()
+        #     loss.backward()
+        #     optimizer.step()
+        #     avg_loss = total_loss / len(dataloader)
+        #     print(f"Epoch [{epoch+1}/{20}] Loss: {avg_loss}")
 
-                    # Generate fake images using fixed trained generator
-                    noise = torch.randn(real.shape[0], NOISE_DIM, 1, 1).to(device)
-                    fake = netG(noise)
-                    
-                    # save fake as an image in a folder, along with noise in a grid
-                    vutils.save_image(fake.detach(), f"results/fake_{epoch}_{batch_idx}.png", normalize=True)
-                    # vutils.save_image(noise.detach(), f"results/noise_{epoch}_{batch_idx}.png", normalize=True)
+        encoder.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for _, (real, _) in enumerate(dataloader_val):
+                real = real.to(device)
+                noise = encoder(real).view(-1, 100, 1, 1)
+                fake = netG(noise)
+                val_loss += criterion(real, fake).item()
+        avg_val_loss = val_loss / len(dataloader_val)
+        print(f"Validation Loss: {avg_val_loss}")
 
-                    # Encode fake images
-                    encoded_fake = encoder(fake)
+        fig, axs = plt.subplots(2, 5, figsize=(15, 6))
+        for j in range(5):
+            axs[0, j].imshow(real[j].detach().cpu().numpy().transpose(1, 2, 0))
+            axs[0, j].axis("off")
+            axs[1, j].imshow(fake[j].detach().cpu().numpy().transpose(1, 2, 0))
+            axs[1, j].axis("off")
 
-                    # Compute reconstruction loss, mse between input image feed to encoder and output image given by the gan generator
-                    loss = criterion(encoded_fake, noise.view(noise.size(0), -1))
-                    total_loss += loss.item()
+        plt.tight_layout()
+        plt.savefig(f"results_enc/images_{epoch}.png")
+        plt.close()
+        print(f"Images saved for epoch {epoch}")
 
-                    # Backpropagation
-                    loss.backward()
-                    optimizer.step()
+    torch.save(encoder.state_dict(), "./checkpoints/encoder.pth")
 
-                avg_loss = total_loss / len(dataloader)
-                print(f"Epoch [{epoch+1}/{NUM_EPOCHS}], LR: {lr}, Beta1: {beta1}, Avg Loss: {avg_loss}")
 
-                # Validation
-                encoder.eval()
-                val_loss = 0
-
-                with torch.no_grad():
-                    for _, (real, _) in enumerate(dataloader_val):
-                        real = real.to(device)
-                        noise = torch.randn(real.shape[0], NOISE_DIM, 1, 1).to(device)
-                        fake = netG(noise)
-                        encoded_fake = encoder(fake)
-                        val_loss += criterion(encoded_fake, noise.view(noise.size(0), -1)).item()
-
-                avg_val_loss = val_loss / len(dataloader_val)
-                print(f"Validation Loss: {avg_val_loss}")
-
-                # Save best hyperparameters
-                if avg_val_loss < best_loss:
-                    best_loss = avg_val_loss
-                    best_lr = lr
-                    best_beta1 = beta1
-
-    print(f"Best LR: {best_lr}, Best Beta1: {best_beta1}, Best Validation Loss: {best_loss}")
-    print('--------Finshed Training Encoder--------')
-
-    # saving trained models 
-    torch.save(encoder.state_dict(),"./trained_models/encoder.pth")
-
-    ############################################################################################
-
-if __name__ == '__main__':
-    multiprocessing.freeze_support()
+if __name__ == "__main__":
     main()
